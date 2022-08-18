8bc47b8c9c93a34a7d0cfe42bd579d5d6d04f772
Jhacoby K. Delgado <jkeithd28@gmail.com>
2022-08-17 22:55:26 -0400

    commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,28 @@
 import React from "react";
 import "../style/Form.css"
 
-export const Form = () => {
+export const Form = (data) => {
+    
+    const items = data["data"]["memes"];
+        console.log(items);
+
+    const sample = () => {
+        // var item = items[Math.floor(Math.random()*items.length)];
+        // const items = data["data"]["memes"];
+        // console.log(items);
+        // let item = items[Math.floor(Math.random()*items.length)];
+
+        return 
+    }
+    
     return(
         <form>
             <div className="form">
                 <div className="text-inputs">
-                    <input type={'text'} placeholder='Top Phrase' ></input>
-                    <input type={'text'} placeholder='Bottom Phrase' ></input>
+                    <input type={'text'} ></input>
+                    <input type={'text'} ></input>
                 </div>
-                <button id='button' >Get a new meme image</button>
+                <input type={'submit'} id='button' value='Get a new meme image' ></input>
             </div>
         </form>
     )
