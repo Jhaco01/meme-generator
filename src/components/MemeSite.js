19f1774326501377716e3f633f41dc01b8731872
Jhacoby K. Delgado <jkeithd28@gmail.com>
2022-08-19 21:06:06 -0400

    changing img state button

--- src/components/MemeSite.js
+++ src/components/MemeSite.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { Form } from "./Form";
-import MemeData from "./MemeData";
+import React, { useState } from "react";
+import data from "./MemeData"
+import "../style/MemeSite.css"
 
 export const MemeSite = () => {
+
+    const [url, setUrl] = useState('');
+
+    const getMemeImage = () => {
+
+        const items = data.data.memes;        
+        const item = items[Math.floor(Math.random()*items.length)];
+        setUrl(item.url);
+
+    }
+    
     return(
         <section>
-            < Form 
-                {...MemeData}
-            />
+            <div className="full-site">
+                <div className="form">
+                    <div className="text-inputs">
+                        <input type={'text'} placeholder='Top Phrase' ></input>
+                        <input type={'text'} placeholder='Bottom Phrase' ></input>
+                    </div>
+                    <button id='button' onClick={ getMemeImage } > Get a new meme image </button>                
+                </div>                
+                <img className="img" src={ url } />                
+            </div>
         </section>
     )
 }
