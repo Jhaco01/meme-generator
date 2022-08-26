d9fbeebdbd15a22c26884a0ba08b65b23f9d3324
Jhacoby K. Delgado <jkeithd28@gmail.com>
2022-08-25 22:55:38 -0400

    state changing on inputs added & text on image

--- src/components/MemeSite.js
+++ src/components/MemeSite.js
@@ -8,8 +8,8 @@ export const MemeSite = () => {
 
     const [meme , setMeme] = useState(
         {
-            topText : '',
-            bottomText : '',
+            topPhrase : '',
+            bottomPhrase : '',
             url : '',
         }
     );
@@ -20,18 +20,48 @@ export const MemeSite = () => {
         setMeme(prevState => ({...prevState, url: item.url}));
 
     }
+
+    const addText = ({target}) => {
+        
+        const {name, value} = target;
+        
+        setMeme( (prevState) => {
+            return(
+                {
+                    ...prevState,
+                    [name] : value,
+                }
+            )
+        } )
+    }
     
     return(
         <section>
             <div className="full-site">
                 <div className="form">
                     <div className="text-inputs">
-                        <input type={'text'} placeholder='Top Phrase' ></input>
-                        <input type={'text'} placeholder='Bottom Phrase' ></input>
+                        <input 
+                            type={'text'} 
+                            placeholder='Top Phrase' 
+                            name="topPhrase"
+                            value={meme.topPhrase}
+                            onChange={addText}
+                        />
+                        <input 
+                            type={'text'} 
+                            placeholder='Bottom Phrase' 
+                            name="bottomPhrase"
+                            value={meme.bottomPhrase}
+                            onChange={addText}
+                        />
                     </div>
                     <button id='button' onClick={ getMemeImage } > Get a new meme image </button>                
                 </div>                
-                <img className="img" src={ meme.url } />                
+                <div className="meme" >
+                    <img className="img" src={ meme.url } />                
+                    <h2 id="top" > {meme.topPhrase} </h2>
+                    <h2 id="bottom" > {meme.bottomPhrase} </h2>
+                </div>
             </div>
         </section>
     )
